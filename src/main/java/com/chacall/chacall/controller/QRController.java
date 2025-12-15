46a8bd9281d339a6e88384432ebf97107fd8a165
zoolake <junhomoon224@gmail.com>
2025-12-15 23:25:30 +0900

    chore : api url 클래스 레벨로 변경

--- src/main/java/com/chacall/chacall/controller/QRController.java
+++ src/main/java/com/chacall/chacall/controller/QRController.java
@@ -6,17 +6,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(path = "/api/qrs")
 public class QRController {
 
     private final QRService qrService;
 
-    @GetMapping("/api/qrs/{serialNo}")
+    @GetMapping("/{serialNo}")
     public ResponseEntity<List<ContactResponse>> readContactsByScanningQR(@PathVariable String serialNo) {
         List<ContactResponse> response = qrService.findContactsByQRSerialNo(serialNo).stream()
                 .map(ContactResponse::from)
