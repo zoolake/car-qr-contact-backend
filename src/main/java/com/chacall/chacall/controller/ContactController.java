4a09bfe26d4cba295510accad050d57c2625954d
zoolake <junhomoon224@gmail.com>
2025-12-19 19:12:12 +0900

    feat : 연락처 삭제 API 연동

--- src/main/java/com/chacall/chacall/controller/ContactController.java
+++ src/main/java/com/chacall/chacall/controller/ContactController.java
@@ -1,12 +1,15 @@
 package com.chacall.chacall.controller;
 
+import com.chacall.chacall.auth.SessionUser;
 import com.chacall.chacall.domain.Contact;
 import com.chacall.chacall.dto.request.ContactUpdateRequest;
 import com.chacall.chacall.dto.response.ContactUpdateResponse;
 import com.chacall.chacall.service.ContactService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +22,11 @@ public class ContactController {
     public ResponseEntity<ContactUpdateResponse> updateContact(@PathVariable Long contactId, @RequestBody @Valid ContactUpdateRequest request) {
         Contact contact = contactService.updateContactInfo(contactId, request.getPhoneNumber(), request.getName(), request.getStatus());
         return ResponseEntity.ok(ContactUpdateResponse.from(contact));
+    }
+
+    @DeleteMapping("/{contactId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteContact(@AuthenticationPrincipal SessionUser sessionUser, @PathVariable Long contactId, @RequestParam Long carId) {
+        contactService.deleteContact(sessionUser.getUserId(), carId, contactId);
     }
 }
