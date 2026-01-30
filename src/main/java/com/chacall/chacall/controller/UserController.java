8a5a911be968c981b1838edbd960c5d9c5f08e8a
zoolake <junhomoon224@gmail.com>
2026-01-30 23:24:00 +0900

    chore : https 테스트

--- src/main/java/com/chacall/chacall/controller/UserController.java
+++ src/main/java/com/chacall/chacall/controller/UserController.java
@@ -4,6 +4,7 @@ import com.chacall.chacall.dto.request.UserLoginRequest;
 import com.chacall.chacall.dto.request.UserSignupRequest;
 import com.chacall.chacall.dto.response.UserLoginResponse;
 import com.chacall.chacall.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,15 @@ public class UserController {
 
     /* 로그인 */
     @PostMapping("/login")
-    public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request) {
+    public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request, HttpServletRequest req) {
+
+        System.out.println("req.getScheme() = " + req.getScheme());
+        System.out.println("req.getServerName() = " + req.getServerName());
+        System.out.println("req.getServerPort() = " + req.getServerPort());
+        System.out.println("req.getHeader(\"Host\") = " + req.getHeader("Host"));
+        System.out.println("req.getHeader(\"Origin\") = " + req.getHeader("Origin"));
+        System.out.println("req.getHeader(\"X-Forwarded-Proto\") = " + req.getHeader("X-Forwarded-Proto"));
+
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(request.getPhoneNumber(), request.getPassword());
 
         Authentication authentication = authenticationManager.authenticate(token);
